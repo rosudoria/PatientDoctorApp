549d4765d8f959b5360b8dfeab8c009854f13576
Roswell Doria <rdoria6@my.bcit.ca>
2022-10-25 12:33:33 -0700

    Add project files.

---
+++ PatientDoctorApp/Controllers/DoctorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PatientDoctorApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult DoctorIndex()
+        {
+            return View();
+        }
+    }
+}
---
+++ PatientDoctorApp/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PatientDoctorApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult PatientIndex()
+        {
+            return View();
+        }
+    }
+}
