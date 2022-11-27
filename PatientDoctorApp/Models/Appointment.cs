85d130601b49c823cc10e88aa8daeef2d29b563a
Roswell Doria <rdoria6@my.bcit.ca>
2022-11-26 23:44:35 -0800

    Added Appointment model

---
+++ PatientDoctorApp/Models/Appointment.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatientDoctorApp.Models
+{
+    public class Appointment
+    {
+        public string Details { get; set; }
+
+        [Required(ErrorMessage = "Reason for visting is required")]
+        public string Reason { get; set; }
+
+        [Required(ErrorMessage = "Unique ID")]
+        public string _Id { get; set; }
+
+        [Required(ErrorMessage = "Patient ID required")]
+        public string PatientId { get; set; }
+
+        [Required(ErrorMessage = "Doctor ID required")]
+        public string DoctorId { get; set; }
+
+        [Required(ErrorMessage = "Appointment type required")]
+        public string AppointmentType { get; set; }
+
+        [Required(ErrorMessage = "Status required")]
+        public string Status { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+    }
+
+    enum StatusTypes
+    {
+        PENDING,
+        ACCEPTED,
+        CANCELLED,
+        COMPLEETED
+    }
+
+    enum AppointmentType
+    {
+        INPERSON,
+        ONLINE
+    }
+
+    enum Reasons
+    {
+        FOLLOWUP,
+        RESULTS,
+        OTHER
+    }
+}
