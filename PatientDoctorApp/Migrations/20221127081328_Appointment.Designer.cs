cf8e01e0d491a55e0e6abdc490a00cf55ee7a57b
Roswell Doria <rdoria6@my.bcit.ca>
2022-11-27 00:23:55 -0800

    Added migrations for Appointment Collection

--- PatientDoctorApp/Migrations/20221127081328_Appointment.Designer.cs
+++ PatientDoctorApp/Migrations/20221127081328_Appointment.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PatientDoctorApp.Data;
 
@@ -11,9 +12,10 @@ using PatientDoctorApp.Data;
 namespace PatientDoctorApp.Migrations
 {
     [DbContext(typeof(PatientDoctorAppContext))]
-    partial class PatientDoctorAppContextModelSnapshot : ModelSnapshot
+    [Migration("20221127081328_Appointment")]
+    partial class Appointment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PatientDoctorApp/Migrations/20221127081328_Appointment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PatientDoctorApp.Migrations
+{
+    public partial class Appointment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointment",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    PatientId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    DoctorId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    AppointmentType = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointment");
+        }
+    }
+}
